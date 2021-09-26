501b91e4cfa541ff6b90bc90387e0b082b4b67b3
sarpercode <sarperyn@gmail.com>
2021-09-27 00:30:20 +0300

    just added possible scenarios in order to prevent program from crash

--- getIndeed.py
+++ getIndeed.py
@@ -15,38 +15,41 @@ import os
 '''
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-'''
 
+'''
 
 #For mac run below
 driver = webdriver.Chrome(ChromeDriverManager().install())
 #driver.get("https://ca.indeed.com/jobs?q=computer%20science%20internship&l=Toronto,%20ON&radius=25&ts=1630423938843&pts=1630353837882&rq=1&rsIdx=0")
 
-job = "computer science internship"
-location = "ontario"
+JOB = "machine learning intern"
+LOCATION = "ontario"
 
-companies = []
+company_list = []
 job_list = []
 job_desc_list = []
 
-def go_indeed(job,location):
+
+
+def go_indeed(JOB,LOCATION):
     driver.get("https://ca.indeed.com/")
 
     main = WebDriverWait(driver,10).until(
             EC.presence_of_element_located((By.ID,"ssrRoot"))
         )
+    
+    text_input = main.find_element_by_id("text-input-what")
+    text_input.send_keys(JOB)
+    if len(LOCATION) != 0:
+            LOCATION_input = main.find_element_by_id('text-input-where')
+            LOCATION_input.send_keys(LOCATION)
     try:
-        text_input = main.find_element_by_id("text-input-what")
-        text_input.send_keys(job)
-        if len(location) != 0:
-                location_input = main.find_element_by_id('text-input-where')
-                location_input.send_keys(location)
-        find_jobs = main.find_element_by_xpath('//*[@id="whatWhereFormId"]/div[3]/button')
-        find_jobs.click()
-
+        find_jobs = main.find_element_by_xpath('//*[@id="jobsearch"]/button')
     except:
-        pass
-
+        find_jobs = main.find_element_by_xpath('//*[@id="whatWhereFormId"]/div[3]/button')
+        
+    
+    find_jobs.click()
 
 def create_df(*args):
     df = pd.DataFrame({
@@ -60,32 +63,36 @@ def get_page():
     main = WebDriverWait(driver,10).until(
             EC.presence_of_element_located((By.ID,"resultsBodyContent"))
         )
-    try:
+    
+    # Get company names and JOB titles
+    job_titles = main.find_elements_by_tag_name("h2")
+    company_names = main.find_elements_by_class_name("companyName")
 
-        # Get company names and job titles
-        job_titles = main.find_elements_by_tag_name("h2")
-        company_names = main.find_elements_by_class_name("companyName")
+    # Add the data to each lists
+    for i in range(len(job_titles)):
 
-        #Add the data to each lists
-        for i in range(len(job_titles)):
+        job_titles[i].click()
+        title = job_titles[i].text.replace('new\n','')
+        company = company_names[i].text
+        time.sleep(1.5)
+        try:
+            job_desc = main.find_element_by_id("vjs-desc")
+            #print("try block",job_desc)
+        except:
+            iframe = driver.find_element_by_xpath('//*[@id="vjs-container-iframe"]')
+            driver.switch_to.frame(iframe)
+            job_desc = driver.find_element_by_id("jobDescriptionText")
+            #print("except block",job_desc)
 
-            job_titles[i].click()
-            title = job_titles[i].text.replace('new\n','')
-            company = company_names[i].text
-            time.sleep(1.5)
-            vjs_desc = main.find_element_by_id("vjs-desc")
-            job_desc_list.append(vjs_desc.text)
-            job_list.append(title)
-            companies.append(company)
-
-    finally:
-        pass
-
+        job_desc_list.append(job_desc.text)
+        driver.switch_to.default_content()
+        job_list.append(title)
+        company_list.append(company)
 
 def get_data():
 
     try:
-        for i in range(20):
+        for i in range(3):
             time.sleep(1)
             #ignored_exceptions = (NoSuchElementException,StaleElementReferenceException)
 
@@ -107,20 +114,18 @@ def get_data():
 
     finally:
         #Create the dataset
-        df = create_df(companies,job_list,job_desc_list)
-        job_str = ''.join(x for x in job.title() if not x.isspace())
+        df = create_df(company_list,job_list,job_desc_list)
+        job_str = ''.join(x for x in JOB.title() if not x.isspace())
 
-        if len(location) != 0:
-            location_str = ''.join(x for x in location.title() if not x.isspace())
+        if len(LOCATION) != 0:
+            location_str = ''.join(x for x in LOCATION.title() if not x.isspace())
             df.to_csv((f"{job_str}_{location_str}.csv"))
         
         else:
             df.to_csv((f"{job_str}_.csv"))
 
-
-
-        driver.quit()
+        #driver.quit()
 
     
-go_indeed(job,location)
+go_indeed(JOB,LOCATION)
 get_data()
